fd89553b2e6b56ca2609407fa7b3aa49e6a1ca36
sindhuv23 <sindhuvick8@gmail.com>
2022-12-01 14:11:58 +0530

    Added SVI internal user table

--- ctpat-pui/src-ui/src/app/details/svi-tab/svi-panel/svi-panel.component.ts
+++ ctpat-pui/src-ui/src/app/details/svi-tab/svi-panel/svi-panel.component.ts
@@ -1,6 +1,9 @@
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { JoinSviModalComponent } from 'src/app/core/modals/join-svi-modal/join-svi-modal.component';
 import { SviSettingsModalComponent } from 'src/app/core/modals/svi-settings-modal/svi-settings-modal.component';
 import { SviAgreementModalComponent } from 'src/app/core/modals/svi-agreement-modal/svi-agreement-modal.component';
@@ -11,12 +14,32 @@ import { SviSendCertificationEmailModalComponent } from 'src/app/core/modals/svi
   templateUrl: './svi-panel.component.html',
   styleUrls: ['./svi-panel.component.scss']
 })
-export class SviPanelComponent implements OnInit {
+export class SviPanelComponent implements OnInit, AfterViewInit {
+
+  displayedColumns: string[] = ['companyName', 'businessType', 'accountStatus', 'requestStatus', 'requestDate'];
+  private requestsData: any[] = [];
+  public requests = new MatTableDataSource<any>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
 
   constructor(public dialog: MatDialog) { }
   isJoinSVI = false;
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    this.requestsData.push({
+      companyName: 'Green Testing Inc', businessType: 'Importer', accountStatus: 'Certified', requestStatus: 'Sent',
+      requestDate: new Date('06-02-2022')
+    },{
+      companyName: 'Blue Importer Inc', businessType: 'Importer', accountStatus: 'Certified', requestStatus: 'Sent',
+      requestDate: new Date('09-02-2022')
+    });
+
+    this.requests = new MatTableDataSource<any>(this.requestsData);
+    this.requests.paginator = this.paginator;
+    this.requests.sort = this.matSort;
   }
 
   joinSvi(): void {
